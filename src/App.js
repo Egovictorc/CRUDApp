b5ac94cc7f4bd413f92d873018c6995a7badac21
EGO VICTOR <egovictorc@outlook.com>
2019-05-03 23:44:48 +0100

    added editUser component input validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { increment, decrement, reset, delet, currentUser, edit } from './actionCreators/actionCreator';
-import './css/style.css';
-import Table from './components/Table';
-import Form from './components/Form';
-import EditUser from './components/EditUser';
+import React from "react";
+import { connect } from "react-redux";
+import * as actions from "./actionCreators/actionCreator";
+import "./css/style.css";
+import Table from "./components/Table";
+import Form from "./components/Form";
+import EditUser from "./components/EditUser";
 
 const mapStateToProps = state => {
-    const newState = state;
-    return { ...newState}
-}
-const mapDispatchToProps = { edit, increment, decrement, reset, delet, currentUser}
+  const newState = state;
+  return { ...newState };
+};
+const mapDispatchToProps = {
+  edit: actions.edit,
+  increment: actions.increment,
+  decrement: actions.decrement,
+  reset: actions.reset,
+  delet: actions.delet,
+  currentUser: actions.currentUser
+};
 // const mapDispatchToProps = dispatch => {
 //     return {
 //         increment: ()=> dispatch({type: "INCREMENT"}),
@@ -19,40 +26,37 @@ const mapDispatchToProps = { edit, increment, decrement, reset, delet, currentUs
 //     }
 // }
 
-const App = (props)=> {
-console.log(props)
+const App = props => {
+  console.log(props);
 
-const handleDelete = (id)=> {
-    console.log(id)
-    props.delet(id)
-}
+  const handleDelete = id => {
+    console.log(id);
+    props.delet(id);
+  };
 
-const handleEdit = (id)=> {
+  const handleEdit = id => {
     //Change editing state
-    props.edit(id)
-    
-}
+    props.edit(id);
+  };
 
-const handleClick = (id)=> {
+  const handleClick = id => {
     //props.edit(id)
-    props.increment()
-}
-const handleAddUser = ()=> {
-    props.addUser()
-}
+    props.increment();
+  };
+  const handleAddUser = () => {
+    props.addUser();
+  };
 
-    return (
-        <div className="container">
-            <h1 className="header">CRUD APP</h1>
-            {(props.isEditing) ? <EditUser 
-            /> : <Form 
-            />
-            }
-            <Table table={props.table}
-            handleEdit={handleEdit} 
-            delet={handleDelete} />
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h1 className="header">CRUD APP</h1>
+      {props.isEditing ? <EditUser /> : <Form />}
+      <Table table={props.table} handleEdit={handleEdit} delet={handleDelete} />
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actionCreators/actionCreator';
 
 
-
-import { increment, decrement, saveEdit, reset, delet, currentUser, addUser, edit } from '../actionCreators/actionCreator';
 const mapStateToProps = state => {
     const newState = {...state};
     return  newState 
 }
 
-const mapDispatchToProps = {saveEdit, increment, decrement, reset, delet, currentUser, addUser, edit}
+const mapDispatchToProps = {saveEdit: actions.saveEdit, increment: actions.increment, decrement: actions.decrement, reset: actions.reset, delet: actions.delet, currentUser: actions.currentUser, addUser: actions.addUser, edit: actions.edit}
 
 // const EditUser = ( { isEditing, saveEdit, current, currentUser }) => {
 const EditUser = ( { reset, isEditing, saveEdit, current, currentUser }) => {
@@ -22,27 +21,37 @@ const handleChange = (e)=> {
 
 const handleSave = (e)=>{
     e.preventDefault();
+    if (current.name && current.surname ) {
     const id = current.id;
     saveEdit(id)
     reset()
-    console.log(isEditing)
+    } else {
+    const warn = document.getElementById("form__warn");
+    warn.style.display="block";
+    }
+}
+const handleCancel = (e)=> {
+    e.preventDefault();
+    reset();
 }
 
     return (
         <div>
             <form onSubmit={handleSave} className="form">
             <div className="form__group">
-                <label htmlFor="name" className="form__label">name: </label><br />
-                    <input type="text" id="name" className="form__input" name="name" value={current.name} onChange={handleChange}/>
-                    </div>
-
-            <div className="form__group">
-                <label htmlFor="surname" className="form__label"> surname: </label> <br />
-                    <input type="text" name="surname" id="surname" className="form__input" value={current.surname}  onChange={handleChange}/>    
+                <label htmlFor="name" className="form__label">name: </label>
+                    <input type="text" id="name" className="form__input" name="name" value={current.name} onChange={handleChange} placeholder="please enter name" />
             </div>
 
+            <div className="form__group">
+                <label htmlFor="surname" className="form__label"> surname: </label>
+                    <input type="text" name="surname" id="surname" className="form__input" value={current.surname}  onChange={handleChange} placeholder="please enter surname" />    
+            </div>
+            {/* validate input for name and surname */}
+            <p id="form__warn" className="form__warn--name" style={ {display: "none", color: "red", fontSize: "1rem", textTransform: "Lowercase"} }> * Please enter name / surname</p>
+            
             <button className="form__button form__button--save">save</button>
-            <button className="form__button form__button--cancel">cancel</button>
+            <button className="form__button form__button--cancel" onClick={handleCancel}>cancel</button>
         </form>
         </div>
     )
